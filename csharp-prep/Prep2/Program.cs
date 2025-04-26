933b1e19c7ce618ab3029a076c282e5030b505fe
leonardoalva98 <alv22027@byui.edu>
2025-04-26 11:26:09 -0600

    updates

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int gradeAsInt = int.Parse(grade);
+        string gradeLetter;
+
+        if (gradeAsInt >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (gradeAsInt >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (gradeAsInt >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (gradeAsInt >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+
+        if (gradeAsInt >= 70)
+        {
+            Console.WriteLine($"You passed the class with a {gradeLetter}.");
+        }
+        else
+        {
+            Console.WriteLine($"You failed the class with a {gradeLetter}.");
+        }
     }
 }
