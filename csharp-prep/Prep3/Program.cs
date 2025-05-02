93e5de4900a0f24ef5a623f796c578e9c2aca4d2
leonardoalva98 <alv22027@byui.edu>
2025-05-01 18:31:59 -0600

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+    
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 101);
+        int guess;
+        do
+            {
+            Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess > magic)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magic)
+                {
+                    Console.WriteLine("Higher");
+                }
+                } while (guess != magic);
+
+                Console.WriteLine("You guessed it!");
+
+
     }
 }
