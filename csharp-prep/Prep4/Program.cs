d870b385b7cc5715f4363dcd8eec56cd6140bf2e
leonardoalva98 <alv22027@byui.edu>
2025-05-01 18:51:35 -0600

    prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+        int number;
+
+        do
+        {
+            Console.Write("Enter a number? ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0) {
+                numbers.Add(number);}
+
+        } while (number != 0);
+
+        int sum = numbers.Sum();
+        Console.WriteLine($"The sum is {sum}");
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int maxNumber = numbers.Max();
+        Console.WriteLine("The maximum number is: " + maxNumber);
+
     }
 }
